51349f647a9e791bbb4df3613714b1549c5ed124
Pragya Gupta <pragya.gupta012@gmail.com>
2023-11-21 00:18:33 +0530

    added

--- App.js
+++ App.js
@@ -1,77 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import logo from './image/logo.png'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./image/logo.png";
 
 //config-driven UI
 const app = (
-    <a href="/">
-    <img src={logo} alt="logo" className='logo'/>
-    </a>
-) 
-const HeaderComponent = ()=>{
-    return(
-        <div className='navbar'>
-            {app}
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
-const fruits = {
-    name: "Fruit Plaza",
-    image: "https://www.archanaskitchen.com/images/archanaskitchen/world_breakfast/fresh_fruit_bowl_recipe.jpg",
-    cusines: ["Fruits", "Healthy"],
-    rating: "4.2"
-}
-const salad = {
-    name: "Salad Plaza",
-    image: "https://www.twopeasandtheirpod.com/wp-content/uploads/2023/06/Crate-Barrel-Melamine-Bowl.jpg",
-    cusines: ["Salad", "Healthy"],
-    rating: "4.1"
-}
-const Cards = ()=>{
-    return (
-        <div className="card">
-            <img src={fruits.image} alt="Fruits" />
-            <h2>{fruits.name}</h2>
-            <h3>{fruits.cusines.join(", ")}</h3>
-            <h4>{fruits.rating} stars</h4>
-        </div>
-    )
-}
-
-const BodyComponent = () =>{
-    return (
-    <div className='restaurant-list'>
-        <Cards/>
-        <Cards/>
-        <Cards/>
-        <Cards/>
-        <Cards/>
-        <Cards/>
+  <a href="/">
+    <img src={logo} alt="logo" className="logo" />
+  </a>
+);
+const HeaderComponent = () => {
+  return (
+    <div className="navbar">
+      {app}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-    )
-}
+  );
+};
+const data = [
+  {
+    name: "Fruit Plaza",
+    image:
+      "https://www.archanaskitchen.com/images/archanaskitchen/world_breakfast/fresh_fruit_bowl_recipe.jpg",
+    cuisines: ["Fruits", "Healthy"],
+    rating: "4.2",
+  },
+  {
+    name: "Salad Plaza",
+    image:
+      "https://www.twopeasandtheirpod.com/wp-content/uploads/2023/06/Crate-Barrel-Melamine-Bowl.jpg",
+    cuisines: ["Salad", "Healthy"],
+    rating: "4.1",
+  },
+];
 
-const FooterComponent =()=>{
-    return <h3>Footer</h3>
-}
+const Cards = ({image, name, cuisines, rating}) => {
+  return (
+    <div className="card">
+      <img src={image} alt="Fruits" />
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h4>{rating} stars</h4>
+    </div>
+  );
+};
 
-const AppLayout =()=>{
-    return (
+const BodyComponent = () => {
+  return (//passing props 
+    <div className="restaurant-list">
+      <Cards image = {data[0].image} name = {data[0].name} cuisines = {data[0].cuisines} rating = {data[0].rating}/>
+      <Cards name = {data[1].name}/>
+      <Cards name = {data[0].name}/>
+      <Cards name = {data[1].name}/>
+    </div>
+  );
+};
+
+const FooterComponent = () => {
+  return <h3>Footer</h3>;
+};
+
+const AppLayout = () => {
+  return (
     //this empty tag is React.Fragment and it acts like one parent inside JSX
-    <> 
-    <HeaderComponent/>
-    <BodyComponent/>
-    <FooterComponent/>
+    <>
+      <HeaderComponent />
+      <BodyComponent />
+      <FooterComponent />
     </>
-    )
-}
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<AppLayout/>)
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
