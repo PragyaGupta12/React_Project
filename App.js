2318260d892989e95c84c031d416c95ab201c816
Pragya Gupta <pragya.gupta012@gmail.com>
2023-11-25 23:23:05 +0530

    modified

--- App.js
+++ App.js
@@ -250,9 +250,15 @@ const Cards = ({cloudinaryImageId, name, cuisines, avgRating}) => {
 const BodyComponent = () => {
   return (//passing props 
     <div className="restaurant-list">
-      <Cards cloudinaryImageId = {data[0].info.cloudinaryImageId} name = {data[0].info.name} cuisines = {data[0].info.cuisines} avgRating = {data[0].info.avgRating}/>
-      <Cards {...data[1].info}/>      
+      {/* <Cards cloudinaryImageId = {data[0].info.cloudinaryImageId} name = {data[0].info.name} cuisines = {data[0].info.cuisines} avgRating = {data[0].info.avgRating}/>
+      <Cards {...data[1].info}/>       */}
       {/* ... spread operator spreads the objects and it works just like data[0] one */}
+      
+      {
+        data.map(list=>{
+          <Cards {...data[1].info}/>
+        })
+      }
     </div>
   );
 };
