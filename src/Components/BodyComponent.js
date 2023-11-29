8b28869a5ef3bd52657f63db0579e1df19545bf8
Pragya Gupta <pragya.gupta012@gmail.com>
2023-11-30 01:29:51 +0530

    modified

--- src/Components/BodyComponent.js
+++ src/Components/BodyComponent.js
@@ -1,37 +1,52 @@
 import { data } from "../Config"; //named import
-import  CardData from "../Components/Cards.js"; //default import
+import CardData from "../Components/Cards.js"; //default import
 import { useState } from "react"; //named import
 
+function filterData(searchText, filteredData) {
+  const restoData = filteredData.filter((resData) => resData.info.name.includes(searchText)
+  );
+  return restoData;
+}
 const BodyComponent = () => {
   //local state variable in react
-  const [searchText, setSearchText] = useState("KFC") //useState return an array that has the variable and a func to update that variable
-//this KFC is a default value that will display in the search box
-
+  const [filteredData, setfilteredData] = useState(data);
+  const [searchText, setSearchText] = useState(); //useState return an array that has the variable and a func to update that variable
+  //the value passed in useState() is a default value that will display in the search box
   return (
     <>
-    <div className="search-container">
-      <input type="text" 
-      className="search" 
-      placeholder="Search" 
-      value={searchText}
-      onChange={(e)=>{
-        //e.target.value is nothing but the val we type in the search box
-        setSearchText(e.target.value)
-      }}
-      
-      />
-    </div>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            //e is the event property
+            //e.target.value is nothing but the "value" we type in the search box
+            setSearchText(e.target.value); //setSearchText func will take the "value" and update it into searchText variable
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const updatedData = filterData(searchText, filteredData);
+            setfilteredData(updatedData);
+          }}
+        >
+          Search
+        </button>
+      </div>
 
-    <div className="restaurant-list">
-         {/* passing props */}
-      {/* <Cards cloudinaryImageId = {data[0].info.cloudinaryImageId} name = {data[0].info.name} cuisines = {data[0].info.cuisines} avgRating = {data[0].info.avgRating}/>
+      <div className="restaurant-list">
+        {/* passing props */}
+        {/* <Cards cloudinaryImageId = {data[0].info.cloudinaryImageId} name = {data[0].info.name} cuisines = {data[0].info.cuisines} avgRating = {data[0].info.avgRating}/>
         <Cards {...data[1].info}/>       */}
-      {/* ... spread operator spreads the objects and it works just like data[0] one */}
+        {/* ... spread operator spreads the objects and it works just like data[0] one */}
 
-      {data.map((list) => {
-        return <CardData {...list.info} key={list.info.id} />;
-      })}
-    </div>
+        {filteredData.map((list) => {
+          return <CardData {...list.info} key={list.info.id} />;
+        })}
+      </div>
     </>
   );
 };
