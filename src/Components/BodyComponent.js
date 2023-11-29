bb2f4fb4c6ffb62886b2c7e3a6f530783eb7c877
Pragya Gupta <pragya.gupta012@gmail.com>
2023-11-29 23:29:08 +0530

    modified

--- src/Components/BodyComponent.js
+++ src/Components/BodyComponent.js
@@ -3,8 +3,21 @@ import  CardData from "../Components/Cards.js";
 
 const BodyComponent = () => {
   return (
-    //passing props
+    <>
+    <div className="search-container">
+      <input type="text" 
+      className="search" 
+      placeholder="Search" 
+      value={search}
+      onChange={(e)=>{
+        search = console.log(e.target.value)
+      }}
+      
+      />
+    </div>
+
     <div className="restaurant-list">
+         {/* passing props */}
       {/* <Cards cloudinaryImageId = {data[0].info.cloudinaryImageId} name = {data[0].info.name} cuisines = {data[0].info.cuisines} avgRating = {data[0].info.avgRating}/>
         <Cards {...data[1].info}/>       */}
       {/* ... spread operator spreads the objects and it works just like data[0] one */}
@@ -13,6 +26,7 @@ const BodyComponent = () => {
         return <CardData {...list.info} key={list.info.id} />;
       })}
     </div>
+    </>
   );
 };
 
