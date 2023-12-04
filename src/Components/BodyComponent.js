61f569b95ffb9c273fb16e0be329337755e26167
Pragya Gupta <pragya.gupta012@gmail.com>
2023-12-05 00:01:02 +0530

    modified

--- src/Components/BodyComponent.js
+++ src/Components/BodyComponent.js
@@ -1,10 +1,10 @@
-import { data } from "../Config"; //named import
+// import { data } from "../Config"; //named import
 import CardData from "../Components/Cards.js"; //default import
 import { useState, useEffect } from "react"; //named import
 import Shimmer from "./Shimmer.js";
 
-function restaurantData(searchText, restaurants) {
-  const restoData = restaurants.filter((resData) =>
+function restaurantData(searchText, filteredRestaurants) {
+  const restoData = filteredRestaurants.filter((resData) =>
     resData.info.name.includes(searchText)
   );
   return restoData;
@@ -12,7 +12,8 @@ function restaurantData(searchText, restaurants) {
 const BodyComponent = () => {
   //local state variable in react
   // const [restaurants, setRestaurant] = useState(data);
-  const [restaurants, setRestaurant] = useState([]);
+  const [allRestaurants, setAllRestaurant] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurant] = useState([]);
   const [searchText, setSearchText] = useState(); //useState return an array that has the variable and a func to update that variable
   //the value passed in useState() is a default value that will display in the search box
 
@@ -25,13 +26,21 @@ const BodyComponent = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    setRestaurant(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setFilteredRestaurant(
+      json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setAllRestaurant(
+      json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   }
 
-  //Conditional Rendering 
-  //using ternary operator => if condition ? (then) : (else)
+  if (!allRestaurants) return null;//early return. If the restaurant has no data then it returns null
 
-  return restaurants.length === 0 ? (<Shimmer/>) : (
+  //Conditional Rendering
+  //using ternary operator => if condition ? (then) : (else)
+  return allRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="search-container">
         <input
@@ -48,8 +57,8 @@ const BodyComponent = () => {
         <button
           className="search-btn"
           onClick={() => {
-            const updatedData = restaurantData(searchText, restaurants);
-            setRestaurant(updatedData);
+            const updatedData = restaurantData(searchText, filteredRestaurants);
+            setFilteredRestaurant(updatedData);
           }}
         >
           Search
@@ -62,7 +71,7 @@ const BodyComponent = () => {
         <Cards {...data[1].info}/>       */}
         {/* ... spread operator spreads the objects and it works just like data[0] one */}
 
-        {restaurants?.map((list) => {
+        {filteredRestaurants?.map((list) => {
           return <CardData {...list.info} key={list.info.id} />;
         })}
       </div>
