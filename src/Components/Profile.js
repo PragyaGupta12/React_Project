82d6354294f881660696dbb5b7d1a3087691271a
Pragya Gupta <pragya.gupta012@gmail.com>
2023-12-14 23:18:16 +0530

    modified

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,9 +1,27 @@
-const Profile = () => {
-  return (
-    <div className="profile">
-      <h1>Profile Component!</h1>
-    </div>
-  );
-};
+// const Profile = () => {
+//   return (
+//     <div className="profile">
+//       <h1>Profile Component!</h1>
+//     </div>
+//   );
+// };
+
+import React from 'react';
+
+class Profile extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  componentDidMount(){
+    //API calls
+  }
+  render(){
+    return (
+      <div>
+        <h1>Profile Component!</h1>
+      </div>
+    )
+  }
+}
 
 export default Profile;
