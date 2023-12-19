e4ec9791d2ef14524799d33638fc1a954ed81175
Pragya Gupta <pragya.gupta012@gmail.com>
2023-12-20 00:01:43 +0530

    modified

--- src/Components/Instamart.js
+++ src/Components/Instamart.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
-const Section = ({ title, description }) => {
-  const [isVisible, setIsVisible] = useState(false);
+const Section = ({ title, description, isVisible, setIsVisible }) => {
+  //   const [isVisible, setIsVisible] = useState(false); we'll let the parent control this
   return (
     <div className="border border-black p-2 m-2">
       <h3 className="font-bold text-lg">{title}</h3>
       {isVisible ? (
+        //creating accordians
         <button
           className="underline cursor-pointer text-lime-600"
           onClick={() => {
@@ -30,6 +31,11 @@ const Section = ({ title, description }) => {
 };
 
 const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = useState({
+    showAbout: false,
+    showTeam: false,
+    showCareer: false,
+  });
   return (
     <div className="h-auto">
       <h1 className="p-2 m-2">Instamart</h1>
@@ -38,18 +44,42 @@ const Instamart = () => {
         description={
           "In August 2020, Swiggy launched its instant grocery delivery service called Instamart using a network of dark stores. In early 2021, the company closed Swiggy Stores and expanded its operations under Instamart."
         }
+        isVisible={sectionConfig.showAbout}
+        setIsVisible={() => {
+          setSectionConfig({
+            showAbout: true,
+            showTeam: false,
+            showCareer: false,
+          });
+        }}
       />
       <Section
         title={"Team Instamart"}
         description={
           "In August 2020, Swiggy launched its instant grocery delivery service called Instamart using a network of dark stores. In early 2021, the company closed Swiggy Stores and expanded its operations under Instamart."
         }
+        isVisible={sectionConfig.showTeam}
+        setIsVisible={() => {
+          setSectionConfig({
+            showAbout: false,
+            showTeam: true,
+            showCareer: false,
+          });
+        }}
       />
       <Section
         title={"Career"}
         description={
           "In August 2020, Swiggy launched its instant grocery delivery service called Instamart using a network of dark stores. In early 2021, the company closed Swiggy Stores and expanded its operations under Instamart."
         }
+        isVisible={sectionConfig.showCareer}
+        setIsVisible={() => {
+          setSectionConfig({
+            showAbout: false,
+            showTeam: false,
+            showCareer: true,
+          });
+        }}
       />
     </div>
   );
