dd9f77005f561e3a1d996b605bfc945c5017734a
Pragya Gupta <pragya.gupta012@gmail.com>
2023-12-24 22:11:22 +0530

    modified

--- src/Components/undesatndingTestCase.js
+++ src/Components/undesatndingTestCase.js
@@ -1,4 +1,11 @@
-test('sum of two numbers', ()=>{
-    expect(sum(2,3)).tobe(5)
-})
+test("sum of two numbers", () => {
+  expect(sum(2, 3)).tobe(5);
+});
 
+// function sum(a, b) {
+//   return a + b;
+// }
+
+export const sum = (a, b) => {
+  a + b;
+};
