1281a7aa17b6841762a21c19b42d82b1dd4a7969
Pragya Gupta <pragya.gupta012@gmail.com>
2024-02-29 00:01:30 +0530

    modified

--- src/Components/FooterComponent.js
+++ src/Components/FooterComponent.js
@@ -1,5 +1,17 @@
 const FooterComponent = () => {
-    return <h3 className="p-2 m-2">Footer</h3>;
-  };
+  return (
+    <div className="p-2 m-2">
+      <ul >
+        <li>Contact Us</li>
+      </ul>
+      <p>
+        By continuing past this page, you agree to our Terms of Service, Cookie
+        Policy, Privacy Policy and Content Policies. All trademarks are
+        properties of their respective owners. 2023 © HealthyEats™ Ltd. All
+        rights reserved.
+      </p>
+    </div>
+  );
+};
 
-export default FooterComponent
+export default FooterComponent;
